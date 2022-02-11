e23740956568af7e53753fb05c70c81a3df30724
alix <alice.bystrih@yandex.ru>
2022-02-11 21:56:06 +0300

    Add login form data handler

--- server.py
+++ server.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 
 app = FastAPI()
+
+users = {
+    "alex@user.com": {
+        "name": "Alex",
+        "password": "123456",
+        "balance": 100000
+    },
+    "peet@user.com": {
+        "name": "Peet",
+        "password": "234567",
+        "balance": 20000
+    }
+}
 
 
 @app.get('/')
@@ -10,3 +23,15 @@ def index_page():
     with open('templates/login.html', 'r') as f:
         login_page = f.read()
     return Response(login_page, media_type='text/html')
+
+
+@app.post("/login")
+def process_login_page(username: str = Form(...), password: str = Form(...)):
+    user = users.get(username)
+    if not user or user["password"] != password:
+        return Response("Fuck off", media_type='text/html')
+
+    return Response(
+        f"Hello, {user['name']}!<br/>Your balance: {user['balance']}",
+        media_type='text/html'
+    )
